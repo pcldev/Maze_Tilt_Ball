79da3e5f5dc9a738c03deea3ccdf5abb506f1dcc
Tranhung26112003 <tranvanhung261103@gmail.com>
2023-11-04 00:43:25 +0700

    add activity Game rules for users

--- app/src/main/java/com/example/mazetiltball/LuatchoiActivity.java
+++ app/src/main/java/com/example/mazetiltball/LuatchoiActivity.java
@@ -2,13 +2,24 @@ package com.example.mazetiltball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LuatchoiActivity extends AppCompatActivity {
-
+    Button btnBa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luatchoi);
+        btnBa = findViewById(R.id.btnBack);
+        btnBa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LuatchoiActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
